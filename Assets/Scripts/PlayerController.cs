5c28ba06b31e4f4271dbcc1a018cac9d5c86a179
German Gomez <cordobez_07@hotmail.com>
2021-01-29 20:02:01 -0300

    Player Character Base

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(h, 0, v, Space.Self);
+
+    }
+}
