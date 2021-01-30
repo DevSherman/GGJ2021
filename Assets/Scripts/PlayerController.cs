21c49dd2e1d3d3ee7d1d02ef0fbcef6ed27ec612
German Gomez <cordobez_07@hotmail.com>
2021-01-29 21:16:02 -0300

    Player Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 5.0f;
+    public float jumpFactor = 5f;
+    public float distToGround = 1f;
+
+    public bool jumping;
+    public bool moving;
+
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        transform.Translate(h, 0, v, Space.Self);
+        if (v > 0 || h > 0)
+        {
+            transform.Translate(h, 0, v, Space.Self);
+            moving = true;
+        }
+        else moving = false;
 
+        jumping = !IsGrounded();
+    }
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !jumping)
+        {
+            rig.AddForce(new Vector3(0, jumpFactor, 0), ForceMode.Impulse);
+        }
+
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
 }
