58d9811166394ed19afbd03b70be37db183bbe72
DavidAstral <davidastral4d@gmail.com>
2021-01-31 11:32:19 +0100

    UPDATES/PRUEBAS

---
+++ Assets/Scripts/LuzIntermitente.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuzIntermitente : MonoBehaviour
+{
+    Light luzparpadea;
+    public float minEspera;
+    public float maxEspera;
+
+    private void Start()
+    {
+        luzparpadea = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minEspera, maxEspera));
+            luzparpadea.enabled = !luzparpadea.enabled;
+        }
+    }
+}
