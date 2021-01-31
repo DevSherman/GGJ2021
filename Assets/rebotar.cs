54e7f9466714b037ddf8e66106d67b1875146ac6
canavesio41 <32920300+canavesio41@users.noreply.github.com>
2021-01-31 12:09:54 -0300

    winScene and LoseScene

---
+++ Assets/rebotar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rebotar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scale(gameObject, Vector3.zero, 1f).setEase(LeanTweenType.easeInSine).setLoopPingPong();
+        LeanTween.scale(gameObject, Vector3.one, 1f).setEase(LeanTweenType.easeOutSine).setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
